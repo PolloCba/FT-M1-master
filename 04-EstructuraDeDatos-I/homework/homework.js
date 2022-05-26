d73d88060f6cd482df51c2cb48a84a8cdd26be41
AmayaClaudio84 <amayaclaudio84@gmail.com>
2022-05-26 17:42:21 -0300

    Completado 4to Homework

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -15,9 +15,23 @@ Como ejercicio adicional y completamente opcional, al terminar de resolver este 
 */
 
 function nFactorial(n) {
+  if (n === 0 || n === 1){
+    return 1;
+  }else if(n < 0){
+    return 0;
+  }else{
+    return n * nFactorial(n-1);
+  }
 }
 
 function nFibonacci(n) {
+  if (n === 0){
+    return 0;
+  }else if(n === 1){
+    return 1;
+  }else{
+    return nFibonacci(n-1) + nFibonacci(n-2);
+  }
 }
 
 /*
@@ -29,9 +43,23 @@ Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde e
 Pueden utilizar class o función constructora.
 */
 
-function Queue() {
+  class Queue {
+      constructor(){
+    this.array = [];
+      }
 
-}
+    enqueue(e) {
+       return this.array.push(e)
+    }
+
+    dequeue() {
+       return this.array.shift();
+    }
+
+    size() {
+        return this.array.length;
+    }
+  }
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
